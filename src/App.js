34c622b7c2a8ed32ef154186c4e2a8046bf4a53e
Prakash <prakashakrakr@gmail.com>
2023-02-20 20:01:31 +0530

    eslint

--- src/App.js
+++ src/App.js
@@ -1,32 +1,26 @@
-//component name - App 
+//component name - App
 const Pet = (props) => {
-    return React.createElement(
-        'div', 
-        {}, 
-        React.createElement('h1',{},props.name),
-        React.createElement('h1',{},props.animal),
-        React.createElement('h1',{},props.breed)
-    )
-}
+  return React.createElement(
+    "div",
+    {},
+    React.createElement("h1", {}, props.name),
+    React.createElement("h1", {}, props.animal),
+    React.createElement("h1", {}, props.breed)
+  );
+};
 
-
-//component name - App 
+//component name - App
 const App = () => {
-    return React.createElement(
-        "div",
-        {},
-        [
-            React.createElement(Pet,
-                {
-                name: 'luna',
-                animal : 'dog',
-                breed : 'western'
-            }),
-            React.createElement('h1',{}, 'Adopt Me!')
-        ]
-    )
-}
+  return React.createElement("div", {}, [
+    React.createElement(Pet, {
+      name: "luna",
+      animal: "dog",
+      breed: "western",
+    }),
+    React.createElement("h1", {}, "Adopt Me!"),
+  ]);
+};
 
-const container = document.getElementById('root')
-const root = ReactDOM.createRoot(container)
-root.render(React.createElement(App))
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container);
+root.render(React.createElement(App));
